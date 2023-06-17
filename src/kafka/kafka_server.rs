93671fdc3527dfecc2c262b45fac5451cbaf9251
donald <donald199209@gmail.com>
2023-06-17 08:45:22 +0800

    add kafka_server

--- src/kafka/kafka_server.rs
+++ src/kafka/kafka_server.rs
@@ -5,7 +5,7 @@ use crate::{
 use async_trait::async_trait;
 use core::panic;
 use serde_json::json;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use super::storage::Storage;
 
@@ -13,6 +13,7 @@ use super::storage::Storage;
 pub struct KafkaServer {
     inner: ServerInner,
     storage: Option<Storage>,
+    commit_offsets: HashMap<String, usize>,
 }
 
 impl KafkaServer {
@@ -75,6 +76,67 @@ impl KafkaServer {
             body,
         }
     }
+
+    /**
+     * This message informs the node that messages have been successfully
+     * processed up to and including the given offset.
+     * Your node will receive a request message body that looks like this:
+     * {
+     *      "type": "commit_offsets",
+     *       "offsets": {
+     *          "k1": 1000,
+     *          "k2": 2000
+     *      }
+     * }
+     */
+    pub async fn commit_offsets(&mut self, msg: &Message) -> Message {
+        let offsets = msg.body.payload.get_raw("offsets").clone();
+        let offsets: HashMap<String, usize> = serde_json::from_value(offsets).unwrap();
+        self.merge_offsets(&offsets);
+
+        Message {
+            src: self.inner.node_id().to_string(),
+            dst: msg.src.to_string(),
+            body: Body { 
+                kind: BodyKind::CommitOffsetsOk,
+                msg_id: self.inner.next_msg_id(),
+                reply_to: Some(msg.body.msg_id),
+                payload: Payload::default(),
+            },
+        }
+    }
+
+    fn merge_offsets(&mut self, offsets: &HashMap<String, usize>) {
+        for (key, offset) in offsets.iter() {
+            self.commit_offsets.insert(key.to_string(), *offset);
+        }
+    }
+
+    /**
+     * This message returns a map of committed offsets for a given set of logs.
+     * Clients use this to figure out where to start consuming from in a given log.
+     */
+    pub async fn list_committed_offsets(&mut self, msg: &Message) -> Message {
+        let keys = msg.body.payload.get_raw("keys").clone();
+        let keys: Vec<String> = serde_json::from_value(keys).unwrap();
+        let keys: HashSet<String> = HashSet::from_iter(keys);
+
+        let filtered: HashMap<&String, &usize> = self.commit_offsets
+            .iter()
+            .filter(|&(k,_)| keys.contains(k))
+            .collect();
+
+        Message {
+            src: self.inner.node_id().to_string(),
+            dst: msg.src.to_string(),
+            body: Body { 
+                kind: BodyKind::ListCommittedOffsetsOk,
+                msg_id: self.inner.next_msg_id(),
+                reply_to: Some(msg.body.msg_id),
+                payload: Payload::init("offsets", json!(filtered)),
+            },
+        }
+    }
 }
 
 #[async_trait]
@@ -83,11 +145,13 @@ impl Serve for KafkaServer {
         match msg.body.kind {
             BodyKind::Init => {
                 let reply_msg = self.inner.init(msg);
-                self.storage = Some(Storage::new(self.inner.node_id()).await);
+                self.storage = Some(Storage::new().await);
                 reply_msg
             }
             BodyKind::Send => Some(self.send(msg).await),
             BodyKind::Poll => Some(self.poll(msg).await),
+            BodyKind::CommitOffsets => Some(self.commit_offsets(msg).await),
+            BodyKind::ListCommittedOffsets => Some(self.list_committed_offsets(msg).await),
             _ => panic!("{}", format!("cannot handle msg: {:?}", msg)),
         }
     }
@@ -128,7 +192,7 @@ mod tests {
             .insert("key", json!("k1"))
             .insert("msg", json!(123))
             .build();
-        let reply_msg = server.send(&msg).await;
+        let reply_msg = server.reply(&msg).await.unwrap();
 
         assert_eq!(BodyKind::SendOk, reply_msg.body.kind);
         assert_eq!(0, server.storage().offsets()["k1"]);
@@ -136,7 +200,8 @@ mod tests {
         let buffer = buffer.trim_end();
         assert_eq!("0:k1:123", buffer);
 
-        clean_disk_data(&node_id).await;
+        drop(server);
+        clean_disk_data().await;
     }
 
     #[tokio::test]
@@ -155,19 +220,19 @@ mod tests {
             .insert("key", json!("k1"))
             .insert("msg", json!(123))
             .build();
-        server.send(&msg).await;
+        server.reply(&msg).await;
         let msg = builder
             .clone()
             .insert("key", json!("k1"))
             .insert("msg", json!(123))
             .build();
-        server.send(&msg).await;
+        server.reply(&msg).await;
         let msg = builder
             .clone()
             .insert("key", json!("k2"))
             .insert("msg", json!(122))
             .build();
-        server.send(&msg).await;
+        server.reply(&msg).await;
 
         let msg = MessageBuilder::new()
             .bodykind(BodyKind::Poll)
@@ -186,6 +251,67 @@ mod tests {
         assert_eq!([1_usize, 123_usize], msgs["k1"][0]);
         assert_eq!([0_usize, 122_usize], msgs["k2"][0]);
 
-        clean_disk_data(&node_id).await;
+        drop(server);
+        clean_disk_data().await;
+    }
+
+    #[tokio::test]
+    async fn test_commit_offsets() {
+        let node_id = generate_random_node_id();
+        let mut server = KafkaServer::default();
+        let msg = MessageBuilder::new()
+            .insert("node_id", json!(&node_id))
+            .build();
+        server.reply(&msg).await;
+
+        let builder = MessageBuilder::new().bodykind(BodyKind::CommitOffsets);
+
+        let msg = builder
+            .clone()
+            .insert("offsets", json!(HashMap::from([("k1", 1000), ("k2", 2000)])))
+            .build();
+        let reply_msg = server.reply(&msg).await.unwrap();
+        assert_eq!(BodyKind::CommitOffsetsOk, reply_msg.body.kind);
+
+        drop(server);
+        clean_disk_data().await;
+    }
+
+    #[tokio::test]
+    async fn test_list_committed_offsets() {
+        let node_id = generate_random_node_id();
+        let mut server = KafkaServer::default();
+        let msg = MessageBuilder::new()
+            .insert("node_id", json!(&node_id))
+            .build();
+        server.reply(&msg).await;
+
+        let builder = MessageBuilder::new().bodykind(BodyKind::CommitOffsets);
+        let msg = builder
+            .clone()
+            .insert("offsets", json!(HashMap::from([("k1", 1000), ("k2", 2000)])))
+            .build();
+        server.reply(&msg).await;
+
+        let msg = builder
+            .clone()
+            .insert("offsets", json!(HashMap::from([("k3", 2000), ("k2", 2500)])))
+            .build();
+        server.reply(&msg).await;
+
+        let msg = MessageBuilder::new()
+            .bodykind(BodyKind::ListCommittedOffsets)
+            .insert("keys", json!(vec!["k1", "k2"]))
+            .build();
+        let reply_msg = server.reply(&msg).await.unwrap();
+        assert_eq!(BodyKind::ListCommittedOffsetsOk, reply_msg.body.kind);
+
+        let ret_offsets = reply_msg.body.payload.get_raw("offsets");
+        let ret_offsets: HashMap<String, usize> = serde_json::from_value(ret_offsets.clone()).unwrap();
+        assert_eq!(1000, ret_offsets["k1"]);
+        assert_eq!(2500, ret_offsets["k2"]);
+
+        drop(server);
+        clean_disk_data().await;
     }
 }
--- src/kafka/storage.rs
+++ src/kafka/storage.rs
@@ -1,6 +1,7 @@
 use std::fs::OpenOptions as StdOpenOptions;
 use std::{collections::HashMap, io::Write};
 
+use tokio::fs;
 use tokio::{
     fs::File,
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
@@ -12,7 +13,7 @@ use crate::utils;
  * storage logs & retrieve logs
  * append:
  *  incr offset by key & store logs
- *
+ * can have multiple instances
  */
 #[derive(Debug)]
 pub struct Storage {
@@ -20,7 +21,6 @@ pub struct Storage {
     // store next offset to this key
     offsets: HashMap<String, usize>,
     log: File,
-    node_id: String,
 }
 
 impl Drop for Storage {
@@ -41,18 +41,20 @@ impl Storage {
         &self.log_name
     }
 
-    pub async fn new(node_id: &str) -> Self {
+    pub async fn new() -> Self {
         let open_options = utils::rw_open_options();
-        let filename = format!("{}_log", node_id);
+        let filename = "kafka_log"; // TODO
+        fs::create_dir_all("log").await
+            .expect("failed to create log dir");  // TODO
         let path = format!("log/{}", filename);
 
-        let log = open_options.open(path).await.expect("can't open log file");
+        let log = open_options.open(&path).await
+            .expect(&format!("can't open log file {}", &path));
 
         Self {
-            offsets: Self::load_meta(node_id).await,
-            log_name: filename,
+            offsets: Self::load_meta().await,
+            log_name: filename.to_string(),
             log,
-            node_id: node_id.to_string(),
         }
     }
 
@@ -62,7 +64,7 @@ impl Storage {
      * with format: offset:key:msg
      */
     pub async fn append(&mut self, key: &str, msg: usize) -> usize {
-        let offset = match self.offsets.get_mut(key) {
+        let offset = match self.offsets.get_mut(key) { 
             Some(offset) => {
                 *offset += 1;
                 *offset
@@ -98,6 +100,7 @@ impl Storage {
             .open(format!("log/{}", self.log_name))
             .await
             .expect("cannot read log file");
+
 
         let reader = BufReader::new(file);
         let mut lines = reader.lines();
@@ -135,7 +138,7 @@ impl Storage {
         let mut options = StdOpenOptions::new();
         options.read(true).write(true).truncate(true).create(true);
 
-        let mut meta = options.open(Self::meta_filename(&self.node_id)).unwrap();
+        let mut meta = options.open(Self::meta_filename()).unwrap();
         meta.write_all(serde_json::to_string(&self.offsets).unwrap().as_bytes())
             .unwrap();
     }
@@ -143,10 +146,10 @@ impl Storage {
     /**
      * load meta data into offsets
      */
-    async fn load_meta(node_id: &str) -> HashMap<String, usize> {
+    async fn load_meta() -> HashMap<String, usize> {
         let options = utils::rw_open_options();
         let meta = options
-            .open(Self::meta_filename(node_id))
+            .open(Self::meta_filename())
             .await
             .expect("failed to open meta file");
         let offsets = BufReader::new(meta)
@@ -161,8 +164,8 @@ impl Storage {
         }
     }
 
-    fn meta_filename(node_id: &str) -> String {
-        format!("log/{}_meta", node_id)
+    fn meta_filename() -> &'static str {
+        "log/kafka_meta"
     }
 }
 
@@ -174,9 +177,9 @@ pub mod tests {
 
     use super::Storage;
 
-    pub async fn clean_disk_data(node_id: &str) {
-        utils::delete(&format!("log/{}_log", node_id)).await;
-        utils::delete(&format!("log/{}_meta", node_id)).await;
+    pub async fn clean_disk_data() {
+        utils::delete("log/kafka_log").await;
+        utils::delete("log/kafka_meta").await;
     }
 
     #[tokio::test]
@@ -203,6 +206,7 @@ pub mod tests {
         v.sort_by(|a, b| a[0].cmp(&b[0]));
         assert_eq!(&vec![[0, 100], [1, 101]], v);
 
-        clean_disk_data(&node_id).await;
+        drop(storage); // TODO
+        clean_disk_data().await;
     }
 }
